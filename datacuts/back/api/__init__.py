7508f294b917c842e9fa2439a9f7369d8ac96972
FoxyLuxe <foxscripter@gmail.com>
2022-08-29 21:34:03 -0500

    delete datacuts

--- datacuts/back/api/__init__.py
+++
@@ -1,3 +0,0 @@
-from datacuts.api import views
-
-__all__ = ["views"]
--- datacuts/back/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from datacuts.models.user import User
-from datacuts.models.blocklist import TokenBlocklist
-
-
-__all__ = ["User", "TokenBlocklist"]
