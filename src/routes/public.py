b0e9058c2fe8be0ad8c1bdfba689cb5457b12545
Make310 <luisfmarquez1999@gmail.com>
2022-09-22 17:43:08 -0500

    se mejora header y tamaño de las card

--- src/routes/public.py
+++ src/routes/public.py
@@ -1,13 +1,22 @@
+from re import template
 from flask import Blueprint, render_template, url_for
 
 public_blueprint = Blueprint('public_blueprint', __name__)
 
 @public_blueprint.route('/')
 def home():
-    return render_template("public/index.html", title = "Barber")
+    template_vars = { 
+        "title": "Barber",
+        "state": "inicio"
+    }
+    return render_template("public/index.html", **template_vars)
 
 @public_blueprint.route('/salon')
 def salon():
-    return render_template("public/salon.html", title = "Barber - Salon")
+    template_vars = { 
+        "title": "Barber - Salon",
+        "state": "salon"
+    }
+    return render_template("public/salon.html", **template_vars)
  
 
