bc035e98249ee1210862016e37e8615a49fb9f6a
FoxyLuxe <foxscripter@gmail.com>
2022-10-08 09:33:19 -0500

    user login

--- src/__init__.py
+++ src/__init__.py
@@ -1,10 +1,11 @@
 from .routes.routes import home
-from .extentions.extentions import db
+from .extentions.database import db
+from .extentions.userstore import user_datastore
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import Config
-
+from flask_security import Security 
 app = Flask(
     __name__,
     static_url_path = "",
@@ -14,8 +15,10 @@ app = Flask(
 bootstrap =  Bootstrap(app)
 app.config.from_object(Config)
 app.register_blueprint(home)
-db.init_app(app)
 
+
+app.security = Security(app, user_datastore)
+db.init_app(app)
 
 @app.before_first_request
 def create_tables():
--- src/routes/routes.py
+++ src/routes/routes.py
@@ -1,5 +1,14 @@
 from flask import Blueprint
-from src.controllers.functions import *
+from src.controllers.functions import (
+    index,
+    salon,
+    login,
+    register,
+    get_user,
+    get_user_storage,
+    logout,
+    dash,
+)
 
 home = Blueprint('home', __name__, template_folder='templates',
                  static_folder='static')
@@ -8,3 +17,7 @@ home.add_url_rule('/', methods=['GET', 'POST'], view_func=index)
 home.add_url_rule('/salon', methods=['GET', 'POST'], view_func=salon)
 home.add_url_rule('/login', methods=['GET', 'POST'], view_func=login)
 home.add_url_rule('/registro', methods=['GET', 'POST'], view_func=register)
+home.add_url_rule('/user', methods=['GET'], view_func=get_user)
+home.add_url_rule('/userstorage', methods=['GET'], view_func=get_user_storage)
+home.add_url_rule('/logout', methods=['GET', 'POST'], view_func=logout)
+home.add_url_rule('/dash', methods=['GET', 'POST'], view_func=dash)
