23f5a6932a330ee6e5a278b0d3c1efddcca28576
Make310 <luisfmarquez1999@gmail.com>
2022-10-08 18:27:22 -0500

    Finish landing page

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,10 +1,12 @@
 const regis = document.querySelector('#register')
 const login = document.querySelector('#login')
 const out = document.querySelector('#out')
+const reserva = document.querySelector('#reserva')
 
 if (regis) regis.addEventListener('click', goRegister);
 if (login) login.addEventListener('click', goLogin);
 if (out)  out.addEventListener('click', goOut)
+if(reserva) reserva.addEventListener('click', goDash)
 
 
 function goRegister() {
@@ -17,4 +19,8 @@ function goLogin() {
 
 function goOut() {
     location.href = '/logout'
+}
+
+function goDash() {
+    location.href = '/dash'
 }
